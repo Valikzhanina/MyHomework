ba551a7ecccc4f5759d83b284a0ba490645d73cd
1 <a1@1s-MacBook-Pro.local>
2020-12-03 14:58:30 +0200

    add teory

--- advanced js/hw-3/index.js
+++ advanced js/hw-3/index.js
@@ -1,3 +1,9 @@
+//Прототипное наследование работает таким образом, что сщздается общий класс,
+// в котором прописаны самые обобщенные свойста,универсальны для всех потомков,
+//  а каждый последующий класс  или
+// обьект от него расширяет эти свойста, при чем есть возможность пользоваться
+//  свойствами родителя, что уменьшает задвоение кода.
+
 class Employee {
   constructor(name, age, salary) {
     (this._name = name.trim().toLowerCase()),
@@ -8,10 +14,18 @@ class Employee {
     this._name = newName.trim().toLowerCase();
   }
   set age(newAge) {
-    this._age = newAge;
+    if (Number.isInteger(newAge)) {
+      this._age = newAge;
+    } else {
+      this._age = newAge = 30;
+    }
   }
   set salary(newSalary) {
-    this._salary = newSalary;
+    if (Number.isInteger(newSalary)) {
+      this._salary = newSalary;
+    } else {
+      this._age = newSalary = 1500;
+    }
   }
 
   get name() {
@@ -34,9 +48,17 @@ class Programmer extends Employee {
     return this._salary * 3;
   }
 }
-const pro = new Programmer('Anna', 29, 1500, 'ukr');
-console.log(pro);
-console.log(pro.salary);
-pro.name = 'Rf';
-pro.age = 25;
-console.log(pro);
+const programmer1 = new Programmer('Anna', 29, 1500, 'ukr');
+console.log(programmer1);
+console.log(programmer1.salary);
+programmer1.name = 'Kate';
+programmer1.age = 25;
+console.log(programmer1);
+const programmer2 = new Programmer('Max', 30, 2300, 'ukr');
+console.log(programmer2);
+console.log(programmer2.salary);
+programmer2.name = 'Ihor';
+programmer2.age = 32;
+console.log(programmer2);
+programmer2.age = 'hg';
+console.log(programmer2);
